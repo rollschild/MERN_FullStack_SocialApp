f9b9bcd78e2dfa418a978d7af71e037bfb4838e4
rollschild <rollschild@gmail.com>
2018-07-18 11:51:46 -0400

    Add post feed actions & reducers; connected to backend

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/not-found/NotFound";
+import Posts from "./components/posts/Posts";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -94,6 +95,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
