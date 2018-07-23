8588ce029c16d58efc4c2773ed884f3132060bb1
rollschild <rollschild@gmail.com>
2018-07-23 13:01:12 -0400

    Prepared for deployment

--- index.js
+++ index.js
@@ -34,5 +34,17 @@ app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/profiles", profiles);
 
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // everything that gets hit
+  // direct to the react build file
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}.`));
